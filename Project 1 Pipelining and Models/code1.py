cdf0aa4a4d528f1d31a210433127ae00441ee0f9
cjawesomest <cjcalv@gmail.com>
2021-01-27 15:23:57 -0500

    Adds Jupyter notebook; Begins code1 development

--- Project 1 Pipelining and Models/code1.py
+++ Project 1 Pipelining and Models/code1.py
@@ -1,6 +1,17 @@
 # Cameron J. Calv
 # ECE 612: Applied Machine Learning
 # Project 1 
+import os
+import pandas
+
+def load_data():
+    DATA_NAME = "appml-assignment1-dataset.pkl"
+    REF_DIR = "ref"
+    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), REF_DIR, DATA_NAME)
+    with open(data_path, "rb") as data_in:
+        data = pandas.read_pickle(data_in)
+    return [data['X'], data['y']]
 
 if __name__ == "__main__":
+    [data_features, data_labels] = load_data()
     pass
